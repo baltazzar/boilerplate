72f75650c54305c2b422d1dcdb7de3f75870fc9f
Victor Wilson Pinto Bastos <vbastos@sorocaba.sp.gov.br>
2014-03-13 18:05:45 -0300

    Bugfix. Ao inserir novos templates a task amdifyTemplates não executava corretamente.

--- customTasks/amdifyTemplates.js
+++ customTasks/amdifyTemplates.js
@@ -1,13 +1,16 @@
 var fs = require('fs'),
-	templatesFile = fs.readFileSync('application/templates/templates.js', 'UTF8'),
 	startTpl = '//TEMPLATES\ndefine(function(require, exports, module){',
 	endTpl = '});',
+	templatesFile = null,
 	template = null,
 	pattern = /\/\/TEMPLATES/;
 
 module.exports = function(grunt) {
 
 	grunt.registerTask('amdifyTemplates', function() {
+
+		templatesFile = fs.readFileSync('application/templates/templates.js', 'UTF8');
+
 		if(pattern.test(templatesFile)) {
 			template = templatesFile;
 		} else {

41b9672bb283f2b6f5ebabb002368c1542b7b89c
Victor Wilson Pinto Bastos <vbastos@sorocaba.sp.gov.br>
2014-03-14 12:14:01 -0300

    Arquivos em seu estado padrão.

--- base/main.js
+++ base/main.js
@@ -1,36 +1,3 @@
-//START REQUIRE CONFIG
-require.config({
-	"waitSeconds": 0,
-	"urlArgs": "v=@@versao",
-	"paths": {
-		"baseController": "../base/baseController",
-		"dispatcher": "../base/dispatcher",
-		"router": "../base/router",
-		"application": "../application/application",
-		"initializer": "../application/initializer",
-		"collections": "../application/collections",
-		"controllers": "../application/controllers",
-		"models": "../application/models",
-		"templates": "../application/templates/templates",
-		"views": "../application/views",
-		"config": "../config",
-		"backbone.babysitter": "../libs/backbone.babysitter",
-		"backbone": "../libs/backbone",
-		"backbone.wreqr": "../libs/backbone.wreqr",
-		"bootstrap": "../libs/bootstrap",
-		"handlebars.runtime": "../libs/handlebars.runtime",
-		"jquery": "../libs/jquery",
-		"marionette": "../libs/marionette",
-		"underscore": "../libs/underscore"
-	},
-	"shim": {
-		"bootstrap": [
-			"jquery"
-		]
-	}
-});
-//END REQUIRE CONFIG
-
 require(['libs']);
 
 require(['initializer', 'application', 'dispatcher'], function(Initializer, Application, Dispatcher) {

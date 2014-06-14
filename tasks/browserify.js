13b8df5b3579af36674a60b19a17ff18e7a918e0
Victor Bastos <victorwpbastos@gmail.com>
2014-06-14 00:15:57 -0300

    tasks browserify e default

--- tasks/browserify.js
+++ tasks/browserify.js
@@ -1,76 +1,72 @@
 var gulp = require('gulp'),
 	gutil = require('gulp-util'),
-	changed = require('gulp-changed'),
+	livereload = require('gulp-livereload'),
 	plumber = require('gulp-plumber'),
+	tap = require('gulp-tap'),
+	rename = require('gulp-rename'),
 	source = require('vinyl-source-stream'),
 	glob = require('glob'),
-	watchify = require('watchify'),
-	livereload = require('gulp-livereload');
+	browserify = require('browserify');
 
 gulp.task('browserify:libs', function() {
-	var bundler = watchify([], {
-		fast: true,
-		noParse: glob.sync('./libs/*.js')
-	});
+	gulp.src('./base/main.js', {read: false})
+		.pipe(plumber(gutil.log))
+		.pipe(tap(function(file) {
+			var bundler = browserify();
 
-	glob.sync('./libs/*.js').forEach(function(file) {
-		bundler.require(file, {expose: file.split('./libs/')[1].replace('.js', '')});
-	});
+			glob.sync('./libs/*.js').forEach(function(file) {
+				bundler.require(file, {expose: file.split('./libs/')[1].replace('.js', '')});
+			});
 
-	bundler.require('./node_modules/gulp-handlebars/node_modules/handlebars/dist/cjs/handlebars.runtime.js', {expose: 'handlebars.runtime'});
+			bundler.require('./node_modules/gulp-handlebars/node_modules/handlebars/dist/cjs/handlebars.runtime.js', {expose: 'handlebars.runtime'});
 
-	bundler.on('update', bundle);
-
-	function bundle() {
-		return bundler.bundle().pipe(source('libs.js')).pipe(gulp.dest('temp')).pipe(livereload());
-	}
-
-	return bundle();
+			file.contents = bundler.bundle({fast: true, noParse: glob.sync('./libs/*.js')});
+		}))
+		.pipe(rename('libs.js'))
+		.pipe(gulp.dest('temp'))
+		.pipe(livereload());
 });
 
-gulp.task('browserify:templates', ['templates'], function() {
-	var bundler = watchify([], {
-		fast: true
-	});
+gulp.task('browserify:templates', function() {
+	gulp.src('./temp/templates_compilados.js', {read: false})
+		.pipe(plumber(gutil.log))
+		.pipe(tap(function(file) {
+			var bundler = browserify();
 
-	bundler.require('./temp/templates_compilados.js', {expose: 'templates'});
+			bundler.require(file.path, {expose: 'templates'});
 
-	bundler.on('update', bundle);
-
-	function bundle() {
-		return bundler.bundle().pipe(source('templates.js')).pipe(gulp.dest('temp')).pipe(livereload());
-	}
-
-	return bundle();
+			file.contents = bundler.bundle({fast: true});
+		}))
+		.pipe(rename('templates.js'))
+		.pipe(gulp.dest('temp'))
+		.pipe(livereload());
 });
 
 gulp.task('browserify:application', function() {
-	var bundler = watchify(['./base/main.js'], {
-		fast: true,
-		debug: true
-	});
+	gulp.src('./base/main.js', {read: false})
+		.pipe(plumber(gutil.log))
+		.pipe(tap(function(file) {
+			var bundler = browserify(file.path);
 
-	glob.sync('./base/*.js').forEach(function(file) {
-		bundler.require(file, {expose: file.split('./base/')[1].replace('.js', '')});
-	});
+			glob.sync('./base/*.js').forEach(function(file) {
+				bundler.require(file, {expose: file.split('./base/')[1].replace('.js', '')});
+			});
 
-	glob.sync('./application/**/*.js').forEach(function(file) {
-		bundler.require(file, {expose: file.split('./application/')[1].replace('.js', '')});
-	});
+			glob.sync('./application/**/*.js').forEach(function(file) {
+				bundler.require(file, {expose: file.split('./application/')[1].replace('.js', '')});
+			});
 
-	glob.sync('./libs/*.js').forEach(function(file) {
-		bundler.external(file.split('./libs/')[1].replace('.js', ''));
-	});
+			glob.sync('./libs/*.js').forEach(function(file) {
+				bundler.external(file.split('./libs/')[1].replace('.js', ''));
+			});
 
-	bundler.require('./config.js', {expose: 'config'});
-	bundler.external('templates');
-	bundler.external('handlebars.runtime');
+			bundler.require('./config.js', {expose: 'config'});
+			bundler.external('templates');
+			bundler.external('handlebars.runtime');
 
-	function bundle() {
-		return bundler.bundle().pipe(source('application.js')).pipe(gulp.dest('temp')).pipe(livereload());
-	}
-
-	return bundle();
+			file.contents = bundler.bundle({fast: true, debug: true});
+		}))
+		.pipe(rename('application.js'))
+		.pipe(gulp.dest('temp'))
+		.pipe(livereload());
 });
-
-gulp.task('browserify', ['browserify:libs', 'browserify:templates', 'browserify:application']);
--- tasks/default.js
+++ tasks/default.js
@@ -1,9 +1,6 @@
 var gulp = require('gulp');
 
 gulp.task('default', [
-	'watch',
-	'browserify:libs',
-	'browserify:templates',
-	'browserify:application',
-	'server'
+	'server',
+	'watch'
 ]);

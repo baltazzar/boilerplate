e5f52ff12ba7c6bca2a5f486ab1810d25ca648c0
Victor Wilson Pinto Bastos <vbastos@sorocaba.sp.gov.br>
2014-07-07 15:41:16 -0300

    Remoção do uncss.

--- tasks/minify-css.js
+++ tasks/minify-css.js
@@ -2,21 +2,11 @@ var gulp = require('gulp'),
 	pkg = require('../package.json'),
 	minifyCSS = require('gulp-minify-css'),
 	rename = require('gulp-rename'),
-	glob = require('glob'),
-	uncss = require('gulp-uncss'),
-	uncssFiles = ['index.html'];
+	glob = require('glob');
 
 gulp.task('minify-css', function() {
 
-	glob.sync('./application/templates/**/*.tpl').forEach(function(file) {
-		uncssFiles.push(file);
-	});
-
 	return gulp.src('css/main.css')
-		.pipe(minifyCSS({keepSpecialComments:0}))
-		.pipe(uncss({
-			html: uncssFiles
-		}))
 		.pipe(minifyCSS({keepSpecialComments:0}))
 		.pipe(rename('application.min.css'))
 		.pipe(gulp.dest('dist/' + pkg.version + '/css'));

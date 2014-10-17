c5e470d2e4ed8111b80ba788fe75f223de7a4a48
Victor Wilson Pinto Bastos <vbastos@sorocaba.sp.gov.br>
2014-10-17 14:49:32 -0300

    task default

--- tasks/default.js
+++ tasks/default.js
@@ -1,3 +1,3 @@
 var gulp = require('gulp');
 
-gulp.task('default', ['libs', 'application', 'server', 'watch']);
+gulp.task('default', ['libs', 'partials', 'helpers', 'application', 'server', 'watch']);
